0dbbab6dc432163316055b95f8cdc36716b00880
Joshegend <joshuaokoeguale91@yahoo.com>
2021-08-30 13:54:59 -0700

    me

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
- */
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
+	int c;
+	int d;
+	int e = 0;
 
-while (f < 10)
-{
-	e = 0;
 	while (e < 10)
 	{
 		d = 0;
@@ -24,27 +14,25 @@ while (f < 10)
 			c = 0;
 			while (c < 10)
 			{
-				if (!(f == c && e == d))
+				if (c != d && d != e && e < d && d < c)
 				{
-					putchar('0' + f);
 					putchar('0' + e);
-					putchar(' ');
 					putchar('0' + d);
 					putchar('0' + c);
-					if (!(f + e == 18 && c + d == 17 && d == 9))
+
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+
 				c++;
 			}
 			d++;
 		}
 		e++;
 	}
-	f++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
