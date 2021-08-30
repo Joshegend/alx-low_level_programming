24b63170fe662230fb3451f4875c1664a62ec446
Joshegend <joshuaokoeguale91@yahoo.com>
2021-08-30 13:36:44 -0700

    alt

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(void)
 {
-  int y, x;
-  
-for (x= 0; x<= 9; x++)
-{
-  for (y=x + 1; x<= 9; y++)
-{
-putchar('0'+ x);
-putchar('0' + y);
-If (x != 8 && y != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n'); 
-return (0);
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }
