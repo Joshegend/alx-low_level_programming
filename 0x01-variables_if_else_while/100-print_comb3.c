3097d4eb474265cd353de976c32c963ae3f9e5a1
Joshegend <joshuaokoeguale91@yahoo.com>
2021-08-30 13:04:44 -0700

    finish

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,13 +3,13 @@ int main(void)
 {
   int y, x;
   
-for (x = 0; x<= 9; x++)
+for (x= 0; x<= 9; x++)
 {
-  for (y = x + 1; x <= 9; y++)
+  for (y=x + 1; x<= 9; y++)
 {
 putchar('0'+ x);
 putchar('0' + y);
-if (x != 8 y != 9)
+if (x!= 8 y!= 9)
 {
 putchar(',');
 putchar(' ');
