78b97813e0f858a08c5d76102f188f5d3f47940b
Joshegend <joshuaokoeguale91@yahoo.com>
2021-08-31 11:47:57 -0700

    fish

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
+
 /**
-*main - prints Holberton
-*Return: 0
-*/
+ * main - Entry point
+ * Return:0
+ */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	return (0);
+char c[10] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
