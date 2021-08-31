384d9a85d789d337fd8679e03a32917fa3409226
Joshegend <joshuaokoeguale91@yahoo.com>
2021-08-31 13:53:40 -0700

    goly

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int u;
+
+	for (u = 0; u <= 10; u++)
+	{
+        for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	c++;
+	}
+}
