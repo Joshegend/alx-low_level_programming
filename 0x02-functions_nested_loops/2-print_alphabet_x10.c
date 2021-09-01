aa6c5e2e6b7110353a03a4540eef732955f52da7
Joshegend <joshuaokoeguale91@yahoo.com>
2021-09-01 05:10:58 -0700

    gold

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
+
 /**
- * main - check the code for ALX School students.
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char c;
 	int u;
 
-	for (u = 0; u <= 10; u++)
+	for (u = 0; u <= 9; u++)
 	{
-        for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
