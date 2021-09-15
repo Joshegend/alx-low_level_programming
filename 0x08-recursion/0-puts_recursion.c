601e7179484041273831a47a0ba1431d33f18073
Joshegend <joshuaokoeguale91@yahoo.com>
2021-09-15 13:44:46 -0700

    good

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,19 @@
 #include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: input string
+ *
+ * Description: prints a string, followed by a new line
+ * Return: always 0
+ */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _print_rev_recursion - function that prints the 
- *   reverse of a string
- * @s: argument used
+ * _print_rev_recursion - print reverse
+ * @s: input string
+ *
+ * Description: prints a string in reverse
  * Return: always 0
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
+	{
 		_print_rev_recursion(s + 1);
-	_putchar(*s);
+		_putchar(*s);
+	}
 }
