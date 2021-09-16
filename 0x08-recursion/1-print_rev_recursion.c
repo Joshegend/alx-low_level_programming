9f45a706aa1d6a18bb613f5e6d7cb83592a4210d
Joshegend <joshuaokoeguale91@yahoo.com>
2021-09-16 13:50:57 -0700

    clay

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _print_rev_recursion - print reverse
  * @s: input string
@@ -10,9 +9,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+if (*s > '\0')
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
 }
